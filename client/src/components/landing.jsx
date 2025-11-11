14a5e2f90c16b6ef6b7a27ed441173be720a4144
Farheen Mahjarin Trisha <farheenmahjarin4@gmail.com>
2025-11-11 23:31:42 +0600

    map added

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -203,6 +203,24 @@ export default function LandingPage() {
         </div>
       </section>
 
+      {/* MAP SECTION */}
+<section className="map-section">
+  <h2>Our Location</h2>
+  <div className="map-container">
+    <iframe
+      title="FineAnswer Office Location"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.943502239492!2d90.40158827461542!3d23.750912778686275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7b34277af61%3A0x3263d373deea33c4!2sCo-Desk!5e0!3m2!1sen!2sbd!4v1731336400000!5m2!1sen!2sbd"
+      width="100%"
+      height="400"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+</section>
+
+
       {/* FOOTER */}
       <footer>
         <p>© 2025 FineAnswer Study Abroad Consultancy. All rights reserved.</p>
@@ -228,6 +246,7 @@ export default function LandingPage() {
     </div>
   </div>
 )}
+
 
     </div>
   );
