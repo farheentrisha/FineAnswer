307f3e546c18a34659f7d9abd7f215bf7d631879
niiazmorshed <niazmorshedrafi@gmail.com>
2026-02-22 15:02:40 +0600

    FIxed server

--- server/index.js
+++ server/index.js
@@ -2411,13 +2411,5 @@ app.get("/", (req, res) => {
   res.send("Running Bhaai Running");
 });
 app.listen(port, () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log(`Port Is Running One ${port}`);
-=======
   console.log(`Port Is Running On ${port}`);
->>>>>>> 7f2859af78ac0e96da20da5935c464f272a8ce4f
-=======
-  console.log(`Port Is Running On ${port}`);
->>>>>>> 160d84c375a4ba24d3d76d47236ba17a209b91c4
 });
